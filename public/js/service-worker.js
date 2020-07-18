e7a74c0416484c06318601a55932ec57794c87c1
nicolewallace09 <nicole.elisaw@gmail.com>
2020-07-18 09:41:52 -0700

    files for sw

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,23 +1,23 @@
+// array of files to cache
+const FILES_TO_CACHE = [
+  '../index.html',
+  './idb.js',
+  './index.js',
+  '../css/style.css',
+  '../icons/icon-72x72.png',
+  '../icons/icon-96x96.png',
+  '../icons/icon-128x128.png',
+  '../icons/icon-144x144.png',
+  '../icons/icon-152x152.png',
+  '../icons/icon-192x192.png',
+  '../icons/icon-384x384.png',
+  '../icons/icon-512x512.png',
+];
+
 // global constants 
 const APP_PREFIX = 'BudgetTracker-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
-
-// array of files to cache
-const FILES_TO_CACHE = [
-    '../index.html',
-    './idb.js',
-    './index.js',
-    '../css/style.css',
-    '../icons/icon-72x72.png',
-    '../icons/icon-96x96.png',
-    '../icons/icon-128x128.png',
-    '../icons/icon-144x144.png',
-    '../icons/icon-152x152.png',
-    '../icons/icon-192x192.png',
-    '../icons/icon-384x384.png',
-    '../icons/icon-512x512.png',
-];
 
 // install service worker
 self.addEventListener('install', function (e) {
