4f42e55780a66de873161e4d80e2884afb65e650
nicolewallace09 <nicole.elisaw@gmail.com>
2020-07-18 12:02:48 -0700

    changed back service worker, changed cache files

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,6 +1,7 @@
-// global variables 
-const CACHE_NAME = 'my-site-cache-v1';
-const DATA_CACHE_NAME = 'data-cache-v1';
+// global constants 
+const APP_PREFIX = 'FoodFest-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
   '/',
@@ -19,69 +20,53 @@ const FILES_TO_CACHE = [
   '/icons/icon-512x512.png'  
 ];
 
-// install the service worker
-self.addEventListener('install', function(evt) {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('Your files were pre-cached successfully!');
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-  self.skipWaiting();
-})
-
-// activate the service worker and remove old data from the cache
-self.addEventListener('activate', function(evt) {
-  evt.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log('Removing old cache data', key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
+// adding files to the precache so that the application can use the cache (installing service worker)
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+        console.log('installing cache : ' + CACHE_NAME)
+        return cache.addAll(FILES_TO_CACHE)
+      })
+    )
 });
 
-// intercept fetch requests
-self.addEventListener('fetch', function(evt) {
-  if (evt.request.url.includes('/api/')) {
-    evt.respondWith(
-      caches
-        .open(DATA_CACHE_NAME)
-        .then(cache => {
-          return fetch(evt.request)
-            .then(response => {
-              // if the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
-              }
-
-              return response;
-            })
-            .catch(err => {
-              // network request failed, try to get it from the cache.
-              return cache.match(evt.request);
-            });
-        })
-        .catch(err => console.log(err))
-    );
-    return;
-  }
-  evt.respondWith(
-    fetch(evt.request).catch(function() {
-      return caches.match(evt.request).then(function(response) {
-        if (response) {
-          return response;
-        } else if (evt.request.headers.get('accept').includes('text/html')) {
-          // return the cached home page for all requests for html pages
-          return caches.match('/');
-        }
+// activate a service worker
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
       });
+          cacheKeeplist.push(CACHE_NAME);
+          // returns a promise that resolves once all old versions of the cache have been deleted 
+          return Promise.all(keyList.map(function (key, i) {
+              if (cacheKeeplist.indexOf(key) === -1) {
+              console.log('deleting cache : ' + keyList[i] );
+              return caches.delete(keyList[i]);
+              }
+          })
+      );
+  })
+  )
+});
+
+// retrieving information from the cache 
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+      //   console.log(e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+      //   console.log(e.request.url)
+        return fetch(e.request)
+      }
+
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
     })
-  );
+  )
 });
