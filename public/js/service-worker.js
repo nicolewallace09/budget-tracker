8dbfc4ff0e5bc64fc730c26822bae7d689006cf8
nicolewallace09 <nicole.elisaw@gmail.com>
2020-07-17 19:07:27 -0700

    moved some folders, start of service workers

---
+++ public/js/service-worker.js
@@ -0,0 +1,28 @@
+// global constants 
+const APP_PREFIX = 'Budget-Tracker-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+// array of files to cache
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/css/style.css',
+    '/js/index.js',
+    '/icons/icon-72x72.png',
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png',
+];
+
+// install service worker
+
+// activate service worker
+
+// intercept fetch requests
+
